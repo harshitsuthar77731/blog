a26028d9c3e2189bd9161202c3260a339f6fd495
harshit <harshitsuthar7731@gmail.com>
2021-09-29 14:33:59 +0530

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 //jshint esversion:6
 
+
+//requiring modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
 
+//setting features of modules
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//connecting to mongodb
+mongoose.connect('mongodb://localhost:27017/blogDB');
+
+
+//declaring variables and all
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -12,15 +26,25 @@ const posts = [];
 let  postLink="";
 
 
-const app = express();
+//creating schema
+const postSchema = new mongoose.Schema({
+ postTitle : String,
+ postBody : String
+})
 
-app.set('view engine', 'ejs');
+//creating model
+const Post = mongoose.model("Posts",postSchema);
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
+
+
+
+//get or post 
 app.get('/',function(req,res){
-res.render("home",{hcontent : aboutContent,postToPublish : posts,link:postLink});
+Post.find(function(err,data){
+  res.render("home",{hcontent : aboutContent,postToPublish : data, link:postLink});
+})
+
 })
 
 app.get("/about",function(req,res){
@@ -38,45 +62,35 @@ app.get("/compose",function(req,res){
   res.render("compose");
 
 })
+
+
 app.post("/compose",function(req,res){
- let post ={
-   posttitle: req.body.title,
-   postbody:  req.body.postbody
-   };
-   posts.push(post);
+
+  let title = req.body.title;
+  let content = req.body.postbody;
+  
+  let post = new Post({
+    postTitle: title,
+    postBody:content
+  });
+  post.save();
    res.redirect("/");
 
 })
 
 app.get("/post/:topic",function(req,res){
-  const requestedTitle = lodash.kebabCase(req.params.topic);
 
-  posts.forEach(function(post){
-   if( requestedTitle===lodash.kebabCase(post.posttitle))
-   {
-     res.render("post",
-     {
-       title : post.posttitle,
-       body : post.postbody
-    })
-   }
+  let idkey = req.params.topic;
+  
+  Post.findOne({_id:idkey},function(err,data){
+    if(err)
+    console.log(err)
+    else
+    res.render("post",{ title : data.postTitle, body : data.postBody});
   })
- 
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//listen 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
